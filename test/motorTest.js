2b52ed4fc61b5a2c04f492913bf2ba0c871b5df9
Daniel <danleavitt0@gmail.com>
2016-03-17 19:28:47 -0700

    update move steering

--- test/motorTest.js
+++ test/motorTest.js
@@ -1,7 +1,7 @@
 var test = require('tape')
 
 var WebSocket = require('ws')
-var ws = new WebSocket('ws://192.168.1.221:3000')
+var ws = new WebSocket('ws://192.168.1.83:3000')
 var response = []
 
 ws.on('open', function () {
@@ -11,19 +11,41 @@ ws.on('open', function () {
     ws.send(degrees('c', 2))
     setTimeout(function () {
       t.equals(response[0].ok, true)
+      ws.send(moveSteering(3))
     }, 5000)
   })
 })
 
 ws.on('message', function (data) {
-  console.log(data)
   data = JSON.parse(data)
   if (data['reply_to']) {
     response.push(data)
   }
 })
 
-//
+function moveSteering (ports, id, degrees, speed, turn) {
+  ports = ports || ['b', 'c']
+  speed = speed || 50
+
+  var values = turnToDegrees(turn, speed, degrees)
+  return JSON.stringify({
+    type: 'move_steering',
+    ports: ports,
+    id: id,
+    opts: {
+      left: {
+        'position_sp': values.left.degrees,
+        'duty_cycle_sp': values.left.speed
+      },
+      right: {
+        'position_sp': values.right.degrees,
+        'duty_cycle_sp': values.right.speed
+      }
+    }
+
+  })
+}
+
 function degrees (port, id) {
   return JSON.stringify({
     type: 'motor_write',
@@ -36,30 +58,51 @@ function degrees (port, id) {
     }
   })
 }
-//
-// this.write('position_sp', degrees.toString())
-  // this.write('speed_sp', speed.toString())
-  // this.write('stop_command', braking)
-  // this.write('command', 'run-to-rel-pos')
-//
-// function startRun () {
-//   return {
-//     type: 'motor_write',
-//     value: {
-//       port: 'b',
-//       path: 'command',
-//       value: 'run-forever'
-//     }
-//   }
-// }
-//
-// function stopRun () {
-//   return {
-//     type: 'motor_write',
-//     value: {
-//       port: 'b',
-//       path: 'command',
-//       value: 'reset'
-//     }
-//   }
-// }
+
+
+function turnToSpeeds (turn, speed) {
+  turn = Math.max(Math.min(turn, 100), -100)
+
+  var reducedSpeed = otherSpeed(turn, speed)
+
+  return {
+    left: turn < 0 ? reducedSpeed : speed,
+    right: turn > 0 ? reducedSpeed : speed
+  }
+}
+
+/**
+ * Params for degrees based on turn
+ * @param  {Number} turn
+ * @param  {Number} speed
+ * @param  {Number} degrees
+ * @return {Object} opts    object of degrees and speed for each motor
+ */
+
+function turnToDegrees (turn, speed, degrees) {
+  turn = Math.max(Math.min(turn, 100), -100)
+
+  var opts = turnToSpeeds(turn, speed)
+  opts.left = { speed: opts.left }
+  opts.right = { speed: opts.right }
+
+  var reducedSpeed = otherSpeed(turn, speed)
+  var reducedDegrees = Math.round((reducedSpeed / speed) * degrees)
+  reducedSpeed = Math.abs(reducedSpeed)
+
+  opts.left.degrees = turn < 0 ? reducedDegrees : degrees
+  opts.right.degrees = turn > 0 ? reducedDegrees : degrees
+
+  return opts
+}
+
+/**
+ * Calculate off wheel speed
+ * @param  {Number} turn
+ * @param  {Number} speed
+ * @return {Number} speed of the off motor
+ */
+
+function otherSpeed (turn, speed) {
+  return Math.round(speed * ((100 - (Math.abs(turn) * 2)) / 100))
+}
